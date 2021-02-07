249316c21fe57bc06074e4fbb646dd884b4184ea
Elbarae <elbarae1921@gmail.com>
2021-02-07 15:43:53 +0100

    Added created_at and updated_at columns and triggers to base entity

--- src/Models/BaseModel.ts
+++ src/Models/BaseModel.ts
@@ -1,7 +1,30 @@
-import { PrimaryGeneratedColumn, BaseEntity } from "typeorm";
+import { PrimaryGeneratedColumn, BaseEntity, CreateDateColumn, UpdateDateColumn, BeforeInsert, BeforeUpdate } from "typeorm";
 
 export class BaseModel extends BaseEntity {
 
     @PrimaryGeneratedColumn()
     id!: number;
+
+    @CreateDateColumn({
+        name: "created_at",
+        type: "timestamp"
+    })
+    createdAt: Date;
+
+    @UpdateDateColumn({
+        name: "updated_at",
+        type: "timestamp"
+    })
+    updatedAt: Date;
+
+    @BeforeInsert()
+    beforeInsert() {
+        this.createdAt = new Date();
+        this.updatedAt = this.createdAt;
+    }
+
+    @BeforeUpdate()
+    beforeUpdate() {
+        this.updatedAt = new Date();
+    }
 }
