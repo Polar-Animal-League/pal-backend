201366a8e05bb701c331444c3dba037e5804b339
Renaud <renaudgdumont@gmail.com>
2021-02-07 00:33:54 -0500

    add esmoduleinterop, Typescriptify the imports add routes

---
+++ src/Routes/user.routes.ts
@@ -0,0 +1,10 @@
+import express from "express";
+const router = express.Router()
+import { body, validationResult } from "express-validator";
+
+// define the about route
+router.post('/user/register', function (req, res) {
+
+})
+
+export default router
--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,29 @@
 import { createConnections } from "typeorm";
+import express from "express"
 
-const express = require('express')
 const app = express()
 const port = 8080
+const colors = require('colors/safe');
 
 async function start() {
 
     const connections = await createConnections()
         .then(connection => {
-
+            console.log(colors.green('Database started')); // outputs green text
         });
 }
+
+start();
 
 app.get('/', (req: any, res: any) => {
     res.send('Hello World!')
 })
 
 app.listen(port, '0.0.0.0', () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(colors.green(`PAL-backend listening at http://localhost:${port}`))
 })
 
 
 
 
-start();
 
