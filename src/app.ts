c325ce21034bf4b9837edaa1b57f5040f69b06e7
Renaud <renaudgdumont@gmail.com>
2021-03-12 20:50:53 -0500

    try getting content on /

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import userRoutes from './Routes/user.routes';
 import chalk from 'chalk';
 import cors from "cors"
+import {Request, Response} from "express"
 
 const app = express();
 const port = 8080;
@@ -21,6 +22,10 @@ async function start(): Promise<void> {
         });
     });
 }
+
+app.get("/", function (req : Request, res: Response) {
+    res.sendOkResponse("foobar");
+});
 
 app.use('/user', userRoutes);
 
